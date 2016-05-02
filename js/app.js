68bae178e5824b98d3bc496808f546813a5c4cd4
Devin Barham <barhamd@gmail.com>
2016-05-02 15:06:19 -0600

    refactor and style top

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
 $(document).foundation();
+
+$(function () { 
+    $('#water').highcharts({
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Fruit Consumption'
+        },
+        xAxis: {
+            categories: ['Apples', 'Bananas', 'Oranges']
+        },
+        yAxis: {
+            title: {
+                text: 'Fruit eaten'
+            }
+        },
+        series: [{
+            name: 'Jane',
+            data: [1, 0, 4]
+        }, {
+            name: 'John',
+            data: [5, 7, 3]
+        }]
+    });
+});
