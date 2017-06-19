b5c7990802adcd9539d01d1b04ea40cc145dd0ec
Devin Barham <barhamd@gmail.com>
2017-06-19 23:23:05 +0200

    Only uglify js if production

--- gulpfile.js
+++ gulpfile.js
@@ -11,8 +11,18 @@ const sassPaths = [
   'bower_components/motion-ui/src'
 ];
 
+const jsFiles = [
+  'bower_components/jquery/dist/jquery.js',
+  'bower_components/what-input/what-input.js',
+  'bower_components/foundation-sites/dist/foundation.js',
+  'bower_components/highcharts/highcharts.js',
+  folder.src + 'js/main.js'
+];
+
+let devBuild = (process.env.NODE_ENV !== 'production')
+
 gulp.task('sass', function() {
-  return gulp.src(folder.src + 'scss/main.scss')
+  let cssBuild = gulp.src(folder.src + 'scss/main.scss')
     .pipe($.sass({
       includePaths: sassPaths
     })
@@ -20,23 +30,23 @@ gulp.task('sass', function() {
     .pipe($.autoprefixer({
       browsers: ['last 2 versions', 'ie >= 9']
     }))
-    .pipe($.cleanCss())
-    .pipe(gulp.dest(folder.build + 'css'));
+  if (!devBuild) {
+    cssBuild = cssBuild
+      .pipe($.cleanCss())
+  }
+    return cssBuild.pipe(gulp.dest(folder.build + 'css'));
 });
 
-const jsFiles = [
-  'bower_components/jquery/dist/jquery.js',
-  'bower_components/what-input/what-input.js',
-  'bower_components/foundation-sites/dist/foundation.js',
-  'bower_components/highcharts/highcharts.js',
-  folder.src + 'js/main.js'
-]
-
 gulp.task('js', function() {
-  return gulp.src(jsFiles)
-    .pipe($.concat('main.js'))
-    .pipe($.uglify())
-    .pipe(gulp.dest(folder.build + 'js'));
+  let jsBuild =  gulp.src(jsFiles)
+    .pipe($.concat('main.js'));
+
+  if (!devBuild) {
+    jsBuild = jsBuild
+    .pipe($.uglify());
+  }
+
+    return jsBuild.pipe(gulp.dest(folder.build + 'js'));
 });
 
 gulp.task('haml', function() {
@@ -45,7 +55,9 @@ gulp.task('haml', function() {
   .pipe(gulp.dest(folder.build));
 });
 
-gulp.task('default', ['haml', 'sass', 'js'], function() {
+gulp.task('build', ['haml', 'sass', 'js'])
+
+gulp.task('default', ['build'], function() {
   gulp.watch([folder.src + 'scss/**/*'],  ['sass']);
   gulp.watch([folder.src + 'haml/**/*'],  ['haml']);
   gulp.watch([folder.src + 'js/**/*'],    ['js']);
